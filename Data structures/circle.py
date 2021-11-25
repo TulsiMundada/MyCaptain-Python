9a560ea78fccd508e2e7aacdbbcdbe6186ca9e15
TulsiMundada <89079161+TulsiMundada@users.noreply.github.com>
2021-11-25 16:16:10 +0530

    Create circle.py

---
+++ Data structures/circle.py
@@ -0,0 +1,11 @@
+pi = 3.14
+r = float(input("Enter the radius of the circle: "))
+area = pi * r * r
+print("area of circle of radius",r,"is",area);
+
+
+
+
+x = input("Input the Filename: ")
+f = x.split(".")
+print ("The extension of the file is : " , f) 
